38d3b027bf663a978848cbcba655d3fe1b158095
Nix <nickix94@hotmail.com>
2024-05-05 12:37:15 +0100

    trying this again....

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const app = express();
+
+// Replace these with your actual values
+const CLIENT_ID = process.env.CONSUMER_KEY;
+const CLIENT_SECRET = process.env.CONSUMER_SECRET;
+const REDIRECT_URI = 'http://your-server-domain.com:3000/callback';
+
+// Endpoint to initiate the authorization process
+app.get('/authorize', (req, res) => {
+    const state = crypto.randomBytes(16).toString('hex');
+    // Construct the authorization URL
+    const authorizationUrl = `https://www.tumblr.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&scope=basic&state=${state}&redirect_uri=${REDIRECT_URI}`;
+
+    // Redirect the user to the authorization URL
+    res.redirect(authorizationUrl);
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
